e4609cd740e6a46778b1c87889f5692d2fc52040
Dandeline <Dandeline>
2017-09-24 00:17:41 +0300

    Added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+  var newConfig = [];
+  config.forEach(function(elem) {
+    newConfig.push(elem.join(''));
+  });
+
+  var search= 1, l;
+
+  while(search ) {
+    search = 0;
+    newConfig.forEach(function(elem) {
+      l = str.length;
+      str = str.replace(elem, '' );
+       if( l > str.length) search++;
+    });
+  }
+
+  return str.length == 0;
 }
+
+
